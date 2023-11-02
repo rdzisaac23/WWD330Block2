b0abccddc17b6ffa9b544cc91651204adf4e1bc7
Isaac Rodriguez <rod18056@byui.edu>
2023-11-02 14:12:31 -0600

    W01 Individual Activity: Add Item to Cart Broken

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,14 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.js";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  try {
+    const cartItems = getLocalStorage("so-cart");
+    // console.log(cartItems);
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+} catch (error) {
+  // console.error(error);
+}
 }
 
 function cartItemTemplate(item) {
--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,32 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.js";
+import ProductData from "./ProductData.js";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+  // Retrieve existing cart items from local storage
+  let cartItems = getLocalStorage("so-cart");
+ 
+  // If not array, initialize as empty array
+  if (!Array.isArray(cartItems)) {
+     cartItems = [];
+  }
+ 
+  // Add new product to cartItems array
+  cartItems.push(product);
+ 
+  // Save updated cartItems array back to local storage
+  setLocalStorage("so-cart", cartItems);
+ }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  try {
+    const product = await dataSource.findProductById(e.target.dataset.id);
+    addProductToCart(product);
+  } catch (error) {
+    // console.error(error);
+  }
 }
 
 // add listener to Add to Cart button
